f03edefbd1a5d71f8bda7dac0ee0078dcffb7d99
soichirokando <skando@sonix.asia>
2016-11-09 12:35:17 +0900

    何やかんや

--- PlaceMapTests/PlaceMapTests.swift
+++ PlaceMapTests/PlaceMapTests.swift
@@ -9,35 +9,35 @@
 import XCTest
 @testable import PlaceMap
 class PlaceMapTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let pdb = PlaceDatabase()
-        XCTAssertEqual(pdb.places.count, 47)
-        XCTAssertEqual(pdb.prefectureOf("沖縄県"), "那覇市")
-        
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            let pdb = PlaceDatabase()
-            for _ in 0  ..< 1000 {
-                XCTAssertTrue((pdb.prefectureOf("和歌山県") != nil))
-              
-            }
-        }
-    }
-    
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  //    func testExample()
+  //        // This is an example of a functional test case.
+  //        // Use XCTAssert and related functions to verify your tests produce the correct results.
+  //        let pdb = PlaceDatabase()
+  //        XCTAssertEqual(pdb.places.count, 47)
+  //        XCTAssertEqual(pdb.prefectureOf("沖縄県"), "那覇市")
+  //
+  //    }
+  
+  //    func testPerformanceExample() {
+  //        // This is an example of a performance test case.
+  //        self.measure {
+  //            let pdb = PlaceDatabase()
+  //            for _ in 0  ..< 1000 {
+  //                XCTAssertTrue((pdb.prefectureOf("和歌山県") != nil))
+  //
+  //            }
+  //        }
+  //    }
+  
 }
