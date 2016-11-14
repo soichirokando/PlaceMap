bd89e2c682ebaa54a7ae7b9ca38d0a06a9b43de9
soichirokando <skando@sonix.asia>
2016-11-14 15:28:02 +0900

    Update PlaceMapTests.swift

--- PlaceMapTests/PlaceMapTests.swift
+++ PlaceMapTests/PlaceMapTests.swift
@@ -7,19 +7,7 @@
 //
 
 import XCTest
-@testable import PlaceMap
-class PlaceMapTests: XCTestCase {
-  
-  override func setUp() {
-    super.setUp()
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-  }
-  
-  override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    super.tearDown()
-  }
-  
+
   //    func testExample()
   //        // This is an example of a functional test case.
   //        // Use XCTAssert and related functions to verify your tests produce the correct results.
